d29677d239e87d7dee610b5d5999b3be2a8b13e6
LeeYY26088 <26088@naver.com>
2019-03-09 00:55:52 +0900

    아주 멍멍이 같은 문제

--- src/p1074/Z.java
+++ src/p1074/Z.java
@@ -2,16 +2,37 @@ package p1074;
 
 import java.util.Scanner;
 
-class Path{
-	public int getPath(int n, int x, int y) {
-		if(x<=3&&y<=3) {
-			return ;
-		}			
-		return getPath(n,x,y);
-	}
-}
-
 public class Z {
+
+	static int getPath(int n, int x, int y) {
+		
+		int count=0;
+		int p=(int)Math.pow(2, n-1);
+		
+		if(n==1) {
+			if(x==1)
+				count++;
+			if(y==1)
+				count+=2;
+			
+			return count;
+		}
+		if(x>=p&&y<p) {
+			x-=p;
+			count+=(p*p-1)+1;
+		}
+		else if(x<p&&y>=p) {
+			y-=p;
+			count+=2*((p*p-1)+1);
+		}
+		else if(x>=p&&y>=p) {
+			x-=p;
+			y-=p;
+			count+=3*((p*p-1)+1);
+		}
+		
+		return count+getPath(n-1,x,y);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -25,9 +46,7 @@ public class Z {
 		System.out.print("¿­: ");
 		int y=input.nextInt();
 
-		Path p=new Path();
-
-		System.out.println(p.getPath(n, x, y));
+		System.out.println(getPath(n, x, y));
 
 	}
 
