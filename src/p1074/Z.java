f261ccdea3d53b8045495a5ee142bff7d03f8738
LeeYY26088 <26088@naver.com>
2019-03-09 01:54:43 +0900

    이제 맞았다

--- src/p1074/Z.java
+++ src/p1074/Z.java
@@ -3,50 +3,37 @@ package p1074;
 import java.util.Scanner;
 
 public class Z {
-
-	static int getPath(int n, int x, int y) {
-		
+	static int getPath(int n, int x, int y) {		
 		int count=0;
-		int p=(int)Math.pow(2, n-1);
-		
+		int p=(int)Math.pow(2, n-1);		
 		if(n==1) {
-			if(x==1)
-				count++;
 			if(y==1)
+				count++;
+			if(x==1)
 				count+=2;
 			
 			return count;
 		}
-		if(x>=p&&y<p) {
+		if(x>=p&&y<p) {//2
 			x-=p;
-			count+=(p*p-1)+1;
-		}
-		else if(x<p&&y>=p) {
-			y-=p;
 			count+=2*((p*p-1)+1);
 		}
-		else if(x>=p&&y>=p) {
+		else if(x<p&&y>=p) {//3
+			y-=p;
+			count+=((p*p-1)+1);
+		}
+		else if(x>=p&&y>=p) {//4
 			x-=p;
 			y-=p;
 			count+=3*((p*p-1)+1);
-		}
-		
+		}		
 		return count+getPath(n-1,x,y);
 	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		Scanner input=new Scanner(System.in);
-
 		int n=input.nextInt();
-
 		int x=input.nextInt();
-
 		int y=input.nextInt();
-
 		System.out.println(getPath(n, x, y));
-
 	}
-
 }
